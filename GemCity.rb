2e63b9cad1a64e169ed790a3081c4bb4c3a32ad0
Samuel Priddy <spriddy@covermymeds.com>
2015-12-13 15:14:06 -0500

    Refactor for conciseness

--- GemCity.rb
+++ GemCity.rb
@@ -6,28 +6,18 @@ class GemCity
 
   def initialize
     @thieves = 5
-    @officers = 5
+    @officers = 1
     @population = 50
   end
 
   def happiness_of_town
     # happiness is random... people don't know what they want!
-    happiness_vals = []
     happiness = 0
-
-    @population.times.each do
-      happiness_vals << rand((100 - successful_crime_rate)..100)
-    end
-
-    happiness_vals.each { |value| happiness += value }
+    @population.times { happiness += rand((100 - successful_crime_rate)..100)}
     happiness / 100
   end
 
   def successful_crime_rate
-    if @thieves <= 0 || @officers > @thieves
-      0
-    else
-      100 * (1 - @officers.to_f / @thieves.to_f)
-    end
+    @thieves <= 0 || @officers > @thieves ? 0 : 100 * (1 - @officers / @thieves.to_f)
   end
 end
