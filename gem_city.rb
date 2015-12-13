c83c0d3a93ff0b7dbb963aa9423ea83acb07c2bc
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-12-13 13:29:52 -0500

    added demographics

--- gem_city.rb
+++ gem_city.rb
@@ -2,18 +2,11 @@
 # This is a town riddled with crime, but we can find out how happy the town is
 class GemCity
   attr_reader :population
+  attr_accessor :thieves, :officers
   def initialize
-    @people = { thieves: 5,
-                officers: 1 }
+    @thieves = 5
+    @officers = 1
     @population = 50
-  end
-
-  def thieves(thieves_number = @people[:thieves])
-    @people[:thieves] = thieves_number
-  end
-
-  def officers(officers_number = @people[:officers])
-    @people[:officers] = officers_number
   end
 
   def happiness_of_town
@@ -30,8 +23,6 @@ class GemCity
   end
 
   def successful_crime_rate
-    thieves = @people[:thieves]
-    officers = @people[:officers]
     if thieves <= 0 || officers > thieves
       odds_percent = 0
     else
@@ -46,7 +37,7 @@ class GemCity
     @people.collect do |type, _number|
       demographics[type] = "#{percent_of_pop(type)}%"
     end
-    civilians_pc = 100 - (percent_of_pop(:officers) + percent_of_pop(:thieves))
+    civilians_pc = 100 - (percent_of_pop(officers) + percent_of_pop(thieves))
     demographics[:civilians] = "#{civilians_pc}%"
     demographics
   end
