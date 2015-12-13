5be178c22ae3a7363953ed68f342e91950955b82
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-12-13 13:27:18 -0500

    added demographics

---
+++ gem_city.rb
@@ -0,0 +1,57 @@
+# This class represents the town of GemCity
+# This is a town riddled with crime, but we can find out how happy the town is
+class GemCity
+  attr_reader :population
+  def initialize
+    @people = { thieves: 5,
+                officers: 1 }
+    @population = 50
+  end
+
+  def thieves(thieves_number = @people[:thieves])
+    @people[:thieves] = thieves_number
+  end
+
+  def officers(officers_number = @people[:officers])
+    @people[:officers] = officers_number
+  end
+
+  def happiness_of_town
+    # happiness is random... people don't know what they want!
+    happiness_vals = []
+    happiness = 0
+    @population.times do
+      happiness_vals.push(rand((100 - successful_crime_rate)..100))
+    end
+    happiness_vals.each do |value|
+      happiness += value
+    end
+    happiness / 100
+  end
+
+  def successful_crime_rate
+    thieves = @people[:thieves]
+    officers = @people[:officers]
+    if thieves <= 0 || officers > thieves
+      odds_percent = 0
+    else
+      odds = 1 - officers.to_f / thieves.to_f
+      odds_percent = odds * 100
+    end
+    odds_percent
+  end
+
+  def city_demographics
+    demographics = {}
+    @people.collect do |type, _number|
+      demographics[type] = "#{percent_of_pop(type)}%"
+    end
+    civilians_pc = 100 - (percent_of_pop(:officers) + percent_of_pop(:thieves))
+    demographics[:civilians] = "#{civilians_pc}%"
+    demographics
+  end
+
+  def percent_of_pop(type)
+    (100 * @people[type] / population.to_f).round
+  end
+end
