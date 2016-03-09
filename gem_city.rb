f4d8437d6f7d5af2ecd8954fcd34ae18e0fb345c
Mike Lustig <mlustig@covermymeds.com>
2016-03-09 16:59:49 -0500

    Complete basic challenge.

---
+++ gem_city.rb
@@ -0,0 +1,48 @@
+# This class represents the town of GemCity
+# This is a town riddled with crime but we can find out how happy the town is
+class GemCity
+  attr_reader :population
+  def initialize
+    @people = {
+      thieves: 5,
+      Officers: 1 }
+    @population = 50
+  end
+
+  def thieves(thieves_number = @people[:thieves])
+    @people[:thieves] = thieves_number
+  end
+
+  def officers
+    @people[:Officers]
+  end
+
+  def officers!(officers)
+    @people[:Officers] = officers
+  end
+
+  def happiness_of_town
+    # happiness is random... people don't know what they want!
+    happiness_vals = []
+    happiness = 0
+    (1..@population).each do
+      happiness_vals.push(rand((100 - successful_crime_rate)..100))
+    end
+    happiness_vals.each do |value|
+      happiness += value
+    end
+    happiness / 100
+  end
+
+  def successful_crime_rate
+    thieves = @people[:thieves]
+    officers = @people[:Officers]
+    if thieves <= 0 || officers > thieves
+      odds_percent = 0
+    else
+      odds = 1 - officers.to_f / thieves.to_f
+      odds_percent = odds * 100
+    end
+    odds_percent
+  end
+end
